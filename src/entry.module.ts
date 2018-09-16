0b23a2041abc2011f265b032671e98bf736ef598
Nathan Graule <solarliner@gmail.com>
2018-09-16 22:15:54 +0200

    Change build engine to TSC

---
+++ src/entry.module.ts
@@ -0,0 +1,9 @@
+import Breadcrumbs from "./components/Breadcrumbs";
+import Card from "./components/Card/Card";
+import Dropdown from "./components/Dropdown/Dropdown";
+import Menu from "./components/Menu/Menu";
+import Message from "./components/Message";
+import Modal from "./components/Modal/Modal";
+import Navbar from "./components/Navbar/Navbar";
+
+export { Breadcrumbs, Card, Dropdown, Menu, Message, Modal, Navbar };
