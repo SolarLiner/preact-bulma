7f52bd15a999a6eac91720eac09b4fa5e966c457
Nathan Graule <solarliner@gmail.com>
2018-09-17 20:58:52 +0200

    Set pages as array of numbers

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -3,128 +3,52 @@ import classnames from "classnames";
 
 interface IPaginationProps {
   class?: string;
-  pages: number;
+  pages: number[];
   current: number;
   onPageChange?(page: number): void;
 }
 
 function generatePages(
-  pages: number,
+  pages: number[],
   current: number,
-  onPageChange?: (page: number) => void
+  onPageChange: (page: number) => void
 ) {
-  return [...new Array(pages)].map((_val, idx) => idx + 1).map(idx => (
-    <li>
-      <a
-        class={classnames("pagination-link", {
-          "is-current": idx == current
-        })}
-        aria-label={`Page ${idx}`}
-        onClick={_ev => onPageChange && onPageChange(idx)}
-      >
-        {idx}
-      </a>
-    </li>
-  ));
-}
-
-function generatePageRange(
-  start: number,
-  end: number,
-  current: number,
-  onPageChange?: (page: number) => void
-) {
-  return [...new Array(end - start + 1)]
-    .map((_val, idx) => idx + start)
-    .map(idx => (
-      <li>
-        <a
-          class={classnames("pagination-link", {
-            "is-current": idx == current
-          })}
-          aria-label={`Page ${idx}`}
-          onClick={_ev => onPageChange && onPageChange(idx)}
-        >
-          {idx}
-        </a>
-      </li>
-    ));
+  return pages.map(page => {
+    if (typeof page === "number") {
+      return (
+        <li>
+          <a
+            class={classnames("pagination-link", {
+              "is-current": page == current
+            })}
+            aria-label={`Page ${page}`}
+            onClick={_ev =>
+              onPageChange && page != current && onPageChange(page)
+            }
+          >
+            {page}
+          </a>
+        </li>
+      );
+    } else {
+      return (
+        <li>
+          <span class="pagination-ellipsis">&hellip;</span>
+        </li>
+      );
+    }
+  });
 }
 
 function generatePagination(props: IPaginationProps) {
-  if (props.pages < 5) {
-    const pages = generatePages(props.pages, props.current, props.onPageChange);
-    return <ul class="pagination-list">{pages}</ul>;
-  } else {
-    if (props.current < 4) {
-      const pages = generatePages(5, props.current, props.onPageChange);
-      return (
-        <ul class="pagination-list">
-          {pages}
-          <li>
-            <span class="pagination-ellipsis">&hellip;</span>
-          </li>
-        </ul>
-      );
-    } else if (props.current < props.pages - 4) {
-      const pages = generatePageRange(
-        props.pages - 5,
-        props.pages,
-        props.current,
-        props.onPageChange
-      );
-      return (
-        <ul class="pagination-list">
-          <li>
-            <span class="pagination-ellipsis">&hellip;</span>
-          </li>
-          {pages}
-        </ul>
-      );
-    } else {
-      const pages = generatePageRange(
-        props.current - 2,
-        props.current + 2,
-        props.current,
-        props.onPageChange
-      );
-      return (
-        <ul class="pagination-list">
-          <li>
-            <span class="pagination-ellipsis">&hellip;</span>
-          </li>
-          {pages}
-          <li>
-            <span class="pagination-ellipsis">&hellip;</span>
-          </li>
-        </ul>
-      );
-    }
-  }
+  const pages = generatePages(props.pages, props.current, props.onPageChange);
+  return <ul class="pagination-list">{pages}</ul>;
 }
 
 export default function Pagination(props: RenderableProps<IPaginationProps>) {
   const classes = classnames("pagination", props.class);
   return (
     <nav class={classes} role="pagination" aria-label="pagination">
-      <a
-        class="pagination-previous"
-        disabled={props.current == 0}
-        onClick={_ev =>
-          props.onPageChange && props.onPageChange(props.current - 1)
-        }
-      >
-        Previous
-      </a>
-      <a
-        class="pagination-next"
-        disabled={props.current == props.pages - 1}
-        onClick={_ev =>
-          props.onPageChange && props.onPageChange(props.current + 1)
-        }
-      >
-        Next
-      </a>
       {generatePagination(props)}
     </nav>
   );
--- stories/components/pagination.tsx
+++ stories/components/pagination.tsx
@@ -6,15 +6,49 @@ import Pagination from "../../src/components/Pagination";
 storiesOf("Pagination", module)
   .add("Few pages", () => (
     <div>
-      <Pagination pages={3} current={1} onPageChange={action("pageChanged")} />
-      <Pagination pages={3} current={2} onPageChange={action("pageChanged")} />
-      <Pagination pages={3} current={3} onPageChange={action("pageChanged")} />
+      <Pagination
+        pages={[1, 2, 3]}
+        current={1}
+        onPageChange={action("pageChanged")}
+      />
+      <Pagination
+        pages={[1, 2, 3]}
+        current={2}
+        onPageChange={action("pageChanged")}
+      />
+      <Pagination
+        pages={[1, 2, 3]}
+        current={3}
+        onPageChange={action("pageChanged")}
+      />
     </div>
   ))
   .add("Many pages", () => (
     <div>
-      <Pagination pages={349} current={2} onPageChange={action("pageChanged")} />
-      <Pagination pages={349} current={36} onPageChange={action("pageChanged")} />
-      <Pagination pages={349} current={348} onPageChange={action("pageChanged")} />
+      <Pagination
+        pages={[1, null, 245, 246, 247, null, 349]}
+        current={246}
+        onPageChange={action("pageChanged")}
+      />
+      <Pagination
+        pages={[1, null, 245, 246, 247, null, 349]}
+        current={246}
+        onPageChange={action("pageChanged")}
+      />
+      <Pagination
+        pages={[1, null, 245, 246, 247, null, 349]}
+        current={246}
+        onPageChange={action("pageChanged")}
+      />
+    </div>
+  ))
+  .add("Rounded", () => (
+    <div>
+      <Pagination
+        pages={[2, 3, 4, 5, null]}
+        current={4}
+        class="is-rounded"
+        onPageChange={action("pageChange")}
+      />
     </div>
   ));
