f735fad9ec804b798f66c03b557b986a61bac44b
Nathan Graule <solarliner@gmail.com>
2018-09-19 10:22:23 +0200

    Create simple RSG function for unique dropdown IDs

---
+++ src/utils/rndString.ts
@@ -0,0 +1,12 @@
+export function chr4() {
+  return Math.random().toString(36).slice(-4);
+}
+
+export default function randomString(length?: number) {
+  const chrs = [...new Array(Math.ceil(length / 4))]
+    .map(_ => chr4())
+    .join("");
+  if (length % 4 == 0)
+    return chrs;
+  else return chrs.slice(-length);
+}
