6545eb5ac576c05b50b8f91d162b52be856a25d0
Nathan Graule <solarliner@gmail.com>
2018-12-02 18:48:55 +0100

    Separate form stories from input

---
+++ stories/forms/index.tsx
@@ -0,0 +1,66 @@
+import { storiesOf } from "@storybook/react";
+import { h } from "preact";
+import { Control, Field, HorizontalGroup, Select, SingleInput, TextArea, TextInput } from "../../src/forms";
+
+storiesOf("Forms", module).add("Horizontal form", () => (
+  <div class="container">
+    <HorizontalGroup label="From">
+      <SingleInput placeholder="John Doe" iconsLeft="fas fa-user" />
+      <SingleInput type="email" placeholder="john@doe.com" iconsLeft="fas fa-envelope" />
+    </HorizontalGroup>
+    <HorizontalGroup label=" ">
+      <Field expanded hasAddons>
+        <Control>
+          <a class="button is-static">+44</a>
+        </Control>
+        <Control expanded>
+          <TextInput type="tel" placeholder="6 43 32 21 10" />
+        </Control>
+      </Field>
+    </HorizontalGroup>
+    <HorizontalGroup label="Department">
+      <Control>
+        <Select options={["Business development", "Marketing", "Sales"]} />
+      </Control>
+    </HorizontalGroup>
+    <HorizontalGroup label="Already a member?">
+      <Field narrow>
+        <Control>
+          <label className="radio">
+            <input type="radio" name="member" /> Yes
+          </label>
+        </Control>
+        <Control>
+          <label className="radio">
+            <input type="radio" name="member" /> No
+          </label>
+        </Control>
+      </Field>
+    </HorizontalGroup>
+    <HorizontalGroup label="Subject">
+      <SingleInput
+        color="danger"
+        placeholder="eg. Partnership opportunity"
+        help="This field is required"
+        helpColor="danger"
+      />
+    </HorizontalGroup>
+    <HorizontalGroup label="Body">
+      <Field>
+        <Control>
+          <TextArea placeholder="Explain how we can help you" />
+        </Control>
+      </Field>
+    </HorizontalGroup>
+    <HorizontalGroup label=" "> {/* Empty label to preserve alignment */}
+      <Field hasAddons>
+        <Control>
+          <input class="button is-outlined is-primary" type="submit" value="Submit"/>
+        </Control>
+        <Control>
+          <input class="button is-outlined" type="reset" value="Reset"/>
+        </Control>
+      </Field>
+    </HorizontalGroup>
+  </div>
+));
--- stories/forms/input.tsx
+++ stories/forms/input.tsx
@@ -1,14 +1,10 @@
 import { action, storiesOf } from "@storybook/react";
 import { h } from "preact";
 
-import { Control, Field, SingleInput, TextInput } from "../../src/forms";
+import { Control, Field, HorizontalGroup, Select, SingleInput, TextArea, TextInput } from "../../src/forms";
 
 storiesOf("Forms/Input", module)
-  .addDecorator(story => (
-    <form onSubmit={ev => ev.preventDefault()}>
-      {story()}
-    </form>
-  ))
+  .addDecorator(story => <form onSubmit={ev => ev.preventDefault()}>{story()}</form>)
   .add("Simple", () => (
     <SingleInput
       label="Name"
@@ -19,22 +15,25 @@ storiesOf("Forms/Input", module)
       onInput={action("onInput")}
       iconsLeft="fas fa-user"
     />
-  )).add("Colors", () => (
+  ))
+  .add("Colors / Loading / Rounded", () => (
     <div>
       <SingleInput label="Default Input" />
-      <SingleInput label="Primary input" color="primary" />
-      <SingleInput label="Info input" color="info" />
-      <SingleInput label="Warning input" color="warning" />
+      <SingleInput label="Primary input" color="primary" loading placeholder="Am loading..." />
+      <SingleInput label="Info input" color="info" rounded placeholder="Am rounded!" />
+      <SingleInput label="Warning input" color="warning" loading placeholder="Am loading..." />
       <SingleInput label="Danger input" color="danger" />
     </div>
-  )).add("Types", () => (
+  ))
+  .add("Types", () => (
     <div>
       <SingleInput label="Default" />
       <SingleInput label="Email" type="email" />
       <SingleInput label="Password" type="password" />
       <SingleInput label="Telephone" type="tel" />
     </div>
-  )).add("Addons", () => (
+  ))
+  .add("Addons", () => (
     <Field hasAddons group="center" label="Donation amount" help="Any amount is appreciated! :)">
       <Control>
         <a class="button is-static">$</a>
@@ -46,13 +45,4 @@ storiesOf("Forms/Input", module)
         <a class="button is-primary">Donate !</a>
       </Control>
     </Field>
-  )).add("Horizontal forms", () => (
-    <Field horizontal label="From" help={["Enter your name", "Enter your email"]}>
-      <Control iconsLeft="fas fa-user">
-        <TextInput placeholder="Name"/>
-      </Control>
-      <Control iconsLeft="fas fa-envelope">
-        <TextInput type="email" placeholder="Email address" />
-      </Control>
-    </Field>
-  ))
+  ));
