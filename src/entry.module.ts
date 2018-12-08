9e786368e7f2422a1d5240a470ff9e64c95536e1
Nathan Graule <solarliner@gmail.com>
2018-12-08 14:42:27 +0100

    Export all in module entry file

--- src/entry.module.ts
+++ src/entry.module.ts
@@ -1,9 +1,3 @@
-import Breadcrumbs from "./components/Breadcrumbs";
-import Card from "./components/Card/Card";
-import Dropdown from "./components/Dropdown/Dropdown";
-import Menu from "./components/Menu/Menu";
-import Message from "./components/Message";
-import Modal from "./components/Modal/Modal";
-import Navbar from "./components/Navbar";
-
-export { Breadcrumbs, Card, Dropdown, Menu, Message, Modal, Navbar };
+export * from "./components";
+export * from "./forms";
+export * from "./layout";
