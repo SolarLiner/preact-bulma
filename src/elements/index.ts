68b783887a7baae44e8eb6d3a657c99d4d4ef72d
Nathan Graule <solarliner@gmail.com>
2019-01-13 16:56:25 +0100

    Export elements in module entry file

---
+++ src/elements/index.ts
@@ -0,0 +1,28 @@
+import Box from "./box";
+import Button, { Buttons, InputButton } from "./button";
+import Content from "./content";
+import Delete from "./delete";
+import Icon from "./icon";
+import Image from "./image";
+import Notification from "./notification";
+import Progress from "./progress";
+import Table from "./table";
+import Tag from "./tag";
+import { Subtitle, Title } from "./title";
+
+export {
+  Box,
+  Button,
+  Buttons,
+  InputButton,
+  Content,
+  Delete,
+  Icon,
+  Image,
+  Notification,
+  Progress,
+  Table,
+  Tag,
+  Subtitle,
+  Title
+};
